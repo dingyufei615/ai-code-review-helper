003122e27398ab6c439a9ff9f544ee96b9c0dd5a
dingyufei <dingyufei@dxy.cn>
2025-05-09 15:21:18 +0800

    删除无用文件

--- api/routes/webhook_routes.py
+++
@@ -1,4 +0,0 @@
-# This file has been refactored.
-# Detailed webhooks are now in webhook_routes_detailed.py
-# General webhooks are now in webhook_routes_general.py
-# Helper functions are in webhook_helpers.py
